850627890ef1209482a6814fb520dcd4cf7b98f3
CyprienFOURCROY <cyprien.fourcroy@student-cs.fr>
2026-06-04 22:27:07 +0200

    refactoring

--- src/pipeline/SQL_TYPE_ALONE/SQL_TYPE_creating_gold_answer.py
+++ src/pipeline/SQL_TYPE_ALONE/SQL_TYPE_creating_gold_answer.py
@@ -2,7 +2,7 @@ import sys
 import re
 from pathlib import Path
 
-ROOT = Path(__file__).resolve().parents[2]
+ROOT = Path(__file__).resolve().parents[3]
 sys.path.insert(0, str(ROOT))
 
 import pandas as pd
